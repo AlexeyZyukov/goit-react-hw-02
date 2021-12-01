1b57d11b3f51daa4ca248c2ec4f2907683a7b9e2
Alexey Zyukov <aalex64@ukr.net>
2021-12-01 23:22:44 +0200

    start project 1

---
+++ src/feedback/feedback.js
@@ -0,0 +1,19 @@
+const Feedback = () => {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </div>
+  );
+};
+
+// function Statistics() {
+//     return (
+//         <div>
+//             const Good =
+//         </div>
+//     );
+// }
+
+export default Feedback;
