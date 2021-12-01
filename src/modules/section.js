94b9f8f04563f170c67866db616267fd7d53d933
Alexey Zyukov <aalex64@ukr.net>
2021-12-02 00:08:19 +0200

    section

---
+++ src/modules/section.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import styles from './section.module.css';
+
+export default function Section({ title, children }) {
+  return (
+    <section className={styles.section}>
+      {title && <h2>{title}</h2>}
+      {children}
+    </section>
+  );
+}
+
+Section.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string,
+  Children: PropTypes.node,
+};
